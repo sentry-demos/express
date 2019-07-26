2424be24bcd1d053621e0e23573cc72cd3662195
vudngo <vu@sentry.io>
2019-07-26 10:27:31 -0700

    changed port from 3001 to 80

--- app.js
+++ app.js
@@ -92,6 +92,6 @@ app.get('/handled', function (req, res) {
 // The error handler must be before any other error middleware
 app.use(Sentry.Handlers.errorHandler());
 
-app.listen(3001, function () {
+app.listen(80, function () {
     console.log('CORS-enabled web server listening on port 3001');
 });
